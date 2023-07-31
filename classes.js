e61c42c452b87975564975c7ea2a8096065038f3
Tai Vo <taivt@bap.jp>
2023-07-31 16:34:44 +0700

    learn classes

--- classes.js
+++ classes.js
@@ -1,27 +1,65 @@
-// function Course(name, price) {
-//   this.name = name
-//   this.price = price
-// } 
 
-class Course {
-  constructor(name, price) {
-    this.name = name
-    this.price = price
+class Circle {
+  constructor(radius) {
+      this.radius = radius; 
   }
 
-  getName() {
-    return this.name
+  // These methods will be added to the prototype. 
+  draw() {
   }
 
-  getPrice() {
-    return this.price
+  // This will be available on the Circle class (Circle.parse())
+  static parse(str) {
   }
 }
 
-const phpCourse = new Course('PHP', 1000)
-const jsCourse = new Course('Javascript', 1200)
+// Using symbols to implement private properties and methods
+const _size = Symbol();
+const _draw = Symbol();
 
+class Square {
+  constructor(size) {
+      // "Kind of" private property 
+      this[_size] = size; 
+  }
 
+  // "Kind of" private method 
+  [_draw]() {
+  }
 
-console.log(phpCourse)
-console.log(jsCourse)
+  // By "kind of" I mean: these properties and methods are essentally
+  // part of the object and are accessible from the outside. But accessing
+  // them is hard and awkward. 
+}
+
+// using WeakMaps to implement private properties and methods
+const _width = new WeakMap();
+
+class Rectangle {
+  constructor(width) {
+      _width.set(this, width);
+  }
+
+  draw() {
+      console.log('Rectangle with width' + _width.get(this));
+  }
+}
+
+// WeakMaps give us better protection than symbols. There is no way 
+// to access private members implemented using WeakMaps from the 
+// outside of an object. 
+
+// Inheritance 
+class Triangle extends Shape {
+  constructor(color) {
+      // To call the base constructor 
+      super(color);
+  }
+
+  draw() {
+      // Call the base method 
+      super.draw();
+
+      // Do some other stuff here
+  }
+}
